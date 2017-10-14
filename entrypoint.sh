a1d136dd0981d073dd8cae597baaf49be7e49d62
Randy Hommel <rhommel@infiniticg.com>
2017-10-14 12:37:44 -0700

    Updated entrypoint to allow for using a templated version of database.php rather than modifying the file inline

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+if [ -z "MYSQL_PASSWORD" ]; then
 MYSQL_PASSWORD=$(aws ssm get-parameters --names mysql-password-${STAGE} --with-decryption --query Parameters[0].Value --output text --region us-west-2)
+fi
+if [ -z "MYSQL_HOSTNAME" ]; then
 MYSQL_HOSTNAME=$(aws ssm get-parameters --names mysql-hostname-${STAGE} --with-decryption --query Parameters[0].Value --output text --region us-west-2)
+fi
 
-sed -i \
+sed \
 -e 's/MYSQL_PASSWORD/'${MYSQL_PASSWORD}'/' \
 -e 's/MYSQL_HOSTNAME/'${MYSQL_HOSTNAME}'/' \
-/var/www/html/application/config/database.php
+/var/www/html/application/config/database.php.template > /var/www/html/application/config/database.php
 
 apache2-foreground
