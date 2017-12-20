6c5de2d29e955958f80494721c9051d16e0f2530
Randy Hommel <rhommel@infiniticg.com>
2017-12-19 17:36:41 -0800

    Changed the location of the application directory out of html root

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,11 +15,11 @@ if [ -z "$MYSQL_HOSTNAME" ]; then
 MYSQL_HOSTNAME=$(aws ssm get-parameters --names mysql-hostname-${STAGE} --with-decryption --query Parameters[0].Value --output text --region us-west-2)
 fi
 
-if [ -f /var/www/html/application/config/database.php.template ]; then
+if [ -f /var/www/application/config/database.php.template ]; then
 sed \
 -e 's/MYSQL_PASSWORD/'${MYSQL_PASSWORD}'/' \
 -e 's/MYSQL_HOSTNAME/'${MYSQL_HOSTNAME}'/' \
-/var/www/html/application/config/database.php.template > /var/www/html/application/config/database.php
+/var/www/application/config/database.php.template > /var/www/application/config/database.php
 fi
 
 sed -i -e 's/DNS_NAME/'${DNS_NAME}'/' /etc/apache2/sites-available/000-default.conf
